57dee2b65432f22c4ee1dfdb5c896343a861d612
Nahid-Nur <nahidnurx@gmail.com>
2026-05-05 21:09:38 +0600

    Update Sales and Order Management.sql

--- Sales and Order Management.sql
+++ Sales and Order Management.sql
@@ -1,1 +1,49 @@
+CREATE TABLE sales (
+    sale_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT,
+    sale_date DATE,
+    total_amount DECIMAL(10,2),
+    status VARCHAR(50),
+
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+
+CREATE TABLE sales_details (
+    sale_detail_id INT PRIMARY KEY AUTO_INCREMENT,
+    sale_id INT,
+    product_id INT,
+    quantity INT,
+    price DECIMAL(10,2),
+
+    FOREIGN KEY (sale_id) REFERENCES sales(sale_id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY AUTO_INCREMENT,
+    customer_id INT,
+    order_date DATE,
+    total_amount DECIMAL(10,2),
+    status VARCHAR(50),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+INSERT INTO orders (customer_id, order_date, total_amount, status) VALUES
+(1, '2026-05-03', 7000.00, 'Completed'),
+(2, '2026-05-03', 4500.00, 'Processing');
+
+CREATE TABLE order_details (
+    order_detail_id INT PRIMARY KEY AUTO_INCREMENT,
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    price DECIMAL(10,2),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+INSERT INTO order_details (order_id, product_id, quantity, price) VALUES
+(1, 1, 2, 3500.00),
+(2, 3, 1, 4500.00);
 
